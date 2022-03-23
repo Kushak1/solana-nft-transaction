aa9f22469726df736e09cb810de5a30a6a23959b
Kushak1 <42020387+Kushak1@users.noreply.github.com>
2022-03-23 22:08:56 +0400

    Refactoring
    SPL lib changed, so refactoring was needed

--- devnet.js
+++ devnet.js
@@ -1,110 +1,91 @@
-const web3 =  require('@solana/web3.js');
+const web3 = require('@solana/web3.js');
 const splToken = require('@solana/spl-token');
 
 (async () => {
-    
-    //Create connection to devnet
-    const connection = new web3.Connection(web3.clusterApiUrl("devnet"));
 
-    //Generate keypair and airdrop 1000000000 Lamports (1 SOL)
-    const myKeypair = web3.Keypair.generate();
+  //Create connection to devnet
+  const connection = new web3.Connection(web3.clusterApiUrl('devnet'));
 
-    let airdropSignature = await connection.requestAirdrop(
-        myKeypair.publicKey, 1000000000
+  //Generate keypair and airdrop 1000000000 Lamports (1 SOL)
+  const myKeypair = web3.Keypair.generate();
+
+  let airdropSignature = await connection.requestAirdrop(
+    myKeypair.publicKey, 1000000000
+  );
+
+  await connection.confirmTransaction(airdropSignature);
+  console.log('Solana public address: ' + myKeypair.publicKey.toBase58());
+
+  //Create mint
+  let mint = await splToken.createMint(connection, myKeypair, myKeypair.publicKey, null, 0, web3.Keypair.generate(), splToken.TOKEN_PROGRAM_ID)
+  console.log(mint)
+  console.log('Mint public address: ' + mint.toBase58());
+
+  //Get the token accont of this solana address, if it does not exist, create it
+  let my_token_account = await splToken.getOrCreateAssociatedTokenAccount(
+    connection, 
+    payer = myKeypair, 
+    mint = mint, 
+    owner = myKeypair.publicKey, 
+    commitment = 'finalized', 
+    allowOwnerOffCurve = false, 
+    confirmOptions = null,
+    programId = splToken.TOKEN_PROGRAM_ID, 
+    associatedTokenProgramId = splToken.ASSOCIATED_TOKEN_PROGRAM_ID,
+  )
+  console.log('Token public address: ' + my_token_account.address.toBase58());
+
+  //Minting 100 new tokens to the token address we just created
+  let mint_to_trx = await splToken.mintTo(
+    connection, 
+    payer = myKeypair, 
+    mint = mint, 
+    destination = my_token_account.address, 
+    authority = myKeypair, 
+    amount = 100, 
+    multiSigners = [myKeypair], 
+    confirmOptions = false, 
+    programId = splToken.TOKEN_PROGRAM_ID,
     );
-    
-    await connection.confirmTransaction(airdropSignature);
-    console.log('Solana public address: ' + myKeypair.publicKey.toBase58());
 
-    //Create mint
-    let mint = await splToken.Token.createMint(connection, myKeypair, myKeypair.publicKey, null, 0, splToken.TOKEN_PROGRAM_ID)
+  console.log(mint_to_trx);
+  //Same thing here, creating or getting accout to transfer our nft
+  let nftReciver = web3.Keypair.generate();
+  let reciver_token_account = await splToken.getOrCreateAssociatedTokenAccount(
+    connection, 
+    payer = myKeypair, 
+    mint = mint, 
+    owner = nftReciver.publicKey, 
+    commitment = 'finalized', 
+    allowOwnerOffCurve = false, 
+    confirmOptions = null, 
+    programId = splToken.TOKEN_PROGRAM_ID, 
+    associatedTokenProgramId = splToken.ASSOCIATED_TOKEN_PROGRAM_ID,
+  );
+  console.log(nftReciver.publicKey.toBase58(), 'Reciver addr')
 
-    console.log('Mint public address: ' + mint.publicKey.toBase58());
+  try {
+    await transfer_tokens(myKeypair, connection, 1, reciver_token_account, my_token_account)
+  } catch (error) {
+    console.log(error)
+  }
 
-    //Get the token accont of this solana address, if it does not exist, create it
-    let myToken = await mint.getOrCreateAssociatedAccountInfo(
-        myKeypair.publicKey
-    )
-
-    console.log('Token public address: ' + myToken.address.toBase58());
-
-    //Minting 100 new tokens to the token address we just created
-    await mint.mintTo(myToken.address, myKeypair.publicKey, [], 1);
-
-    let nftReciver = web3.Keypair.generate();
-    console.log(nftReciver.publicKey.toBase58())
-    try{
-        await transfer(mint.publicKey.toBase58(),myKeypair,nftReciver.publicKey.toBase58(),connection,1)
-    }catch(error){
-        console.log(error)
-    }
-        
-    console.log('Done')
-
+  console.log('Done')
 })();
 
-async function transfer(tokenMintAddress, wallet, to, connection, amount) {
-    const mintPublicKey = new web3.PublicKey(tokenMintAddress);    
-  
-    const mintToken = new splToken.Token(
-      connection,
-      mintPublicKey,
-      splToken.TOKEN_PROGRAM_ID,
-      wallet // The wallet owner will pay to transfer and to create recipients associated token account if it does not yet exist.
-    );
-  
-    const fromTokenAccount = await mintToken.getOrCreateAssociatedAccountInfo(
-      wallet.publicKey
-    );
-  
-    const destPublicKey = new web3.PublicKey(to);
-  
-    // Get the derived address of the destination wallet which will hold the custom token
-    const associatedDestinationTokenAddr = await splToken.Token.getAssociatedTokenAddress(
-      mintToken.associatedProgramId,
-      mintToken.programId,
-      mintPublicKey,
-      destPublicKey
-    );
-  
-    const receiverAccount = await connection.getAccountInfo(associatedDestinationTokenAddr);
-  
-    let transaction = await new web3.Transaction();
-    //In case reciver doesn't have associated token account we are going to crate one for him
-    if (receiverAccount === null) {
-  
-        transaction.add( splToken.Token.createAssociatedTokenAccountInstruction(
-          mintToken.associatedProgramId,
-          mintToken.programId,
-          mintPublicKey,
-          associatedDestinationTokenAddr,
-          destPublicKey,
-          wallet.publicKey
-        ));
-  
-    }
-    transaction.add(
-        splToken.Token.createTransferInstruction(
-        splToken.TOKEN_PROGRAM_ID,
-        fromTokenAccount.address,
-        associatedDestinationTokenAddr,
-        wallet.publicKey,
-        [],
-        amount
-      )
-    );
-    // Sign transaction, broadcast, and confirm
-    try{
-        var signature = await web3.sendAndConfirmTransaction(
-        connection,
-        transaction,
-        [wallet]
-    );
-    }catch(error){
-        console.log(error);
-    }
-    
-    console.log("Transcation signature", signature);
-    console.log("Success!");   
-  
-  }
+async function transfer_tokens(wallet, connection, amount, reciver_token_account, from_token_account) {
+  const transfer_trx = await splToken.transfer(
+    connection, 
+    payer = wallet, 
+    source = from_token_account.address, 
+    destination = reciver_token_account.address, 
+    owner = wallet, 
+    amount = amount, 
+    multiSigners = [wallet], 
+    confirmOptions = false, 
+    programId = splToken.TOKEN_PROGRAM_ID,
+    )
+
+  console.log("Transcation signature", transfer_trx);
+  console.log("Success!");
+}
